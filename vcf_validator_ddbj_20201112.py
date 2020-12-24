a32cd909c5aedb60d6483d0c21acf9a6447f2994
Hata Chihiro <chata@nig.ac.jp>
2020-12-24 17:03:42 +0900

    Add files via upload
    Using test data
    vcffile=test_data/Japanese_endometriosis_SNV.sub.20201224.vcf
    gi_list_file=test_data/chr9.mapping.txt
    skip_dense=0
    path_to_fasta=test_data/chr9.fa

--- vcf_validator_ddbj_20201112.py
+++ vcf_validator_ddbj_20201112.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 
-#検討中ここから
 #def parser():
 #$usage = "Usage: dbsnp_vcf_sub_validator.pl -file filename.gz -gi_list gi_list_file -skip_dense 1/0 -getseq_path path -genome_fasta path_to_fasta
-#             (1 - skip density check
-#              0 - check SNP density)
-#ここまで
+#(1 - skip density check 0 - check SNP density)
 
-#-getseq_path GetSeqFromFasta.pl は固定にする？
-# fastaは指定するようにするか、自由か
-
-#bcftoolsを経るならフォーマットチェックは不要？
+#-getseq_path GetSeqFromFasta.pl
 
 import argparse
 
@@ -23,10 +17,15 @@ parser.add_argument('path_to_fasta', help='Write down the path to fasta')
 
 arge = parser.parse_args()
 
-print('vcffile='+args.vcffile)
-print('gi_list_file='+args.gi_list_file)
-print('skip_dense='+args.skip_dense)
-print('path_to_fasta='+args.path_to_fasta)
+print('vcffile='+arge.vcffile)
+print('gi_list_file='+arge.gi_list_file)
+print('skip_dense='+arge.skip_dense)
+print('path_to_fasta='+arge.path_to_fasta)
+
+vcffile = arge.vcffile
+gi_list_file = arge.gi_list_file
+skip_dense = arge.skip_dense
+path_to_fasta = arge.path_to_fasta
 
 #Exist check of imput files or path
 
@@ -36,8 +35,8 @@ import re
 
 vcf_exist = os.path.isfile(vcffile)
 if vcf_exist:
-	line_cnt = sum([1 for _ in open('sample.txt')])
-	if $line_cnt == 1:
+	line_cnt = sum([1 for _ in open(vcffile)])
+	if line_cnt == 1:
 		sys.exit("ERROR 1.0.1: vcf file is empty")
 	else:
 		pass
@@ -58,18 +57,16 @@ else:
 
 #Cheking a imput score of skip_dense
 
-if skip_dense = (0|1) :
+if int(skip_dense) in [0,1] :
 	pass
 else:
 	sys.exit("ERROR 1.0.1: skip_dense can be 0 or 1")
 
-#fasta のチェックを先にしておく必要がある
-fasta_file = open("path_to_fasta") or die("Failed to open the fasta file")
-
-
+#Checking fasta
+fasta_file = open(path_to_fasta) or die("Failed to open the fasta file")
 
 # Open vcfflile on read only
-vcf_data = open("vcfflile") or die("Failed to open the VCF file")
+vcf_data = open(vcffile) or die("Failed to open the VCF file")
 a = 0
 b = 0
 
@@ -78,36 +75,41 @@ previous_pos = 0
 
 for line in vcf_data:
 #    print(line)
-	if line.startswith('##fileformat'): #vcfのバージョンをチェックする
+	if line.startswith('##fileformat'): #Checking VCF version
 		if re.search('4\.[1-3]$', line):
 			a += 1
+			print("pass ##fileformat")
 		else:
 			sys.exit("ERROR 1.0.1: Illegal vcf version")
 	elif line.startswith('##fileDate'):
 		import datetime
-		tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-		filedate = regex.findall(line)
-		if tomorrow.strftime("%Y%m%d") > filedate.strftime("%Y%m%d") #filedateが未来になっているものは不正
+		tomorrow = str(datetime.date.today() + datetime.timedelta(days=1))
+		filedate = str(re.sub("\\D", "", line))
+		if datetime.datetime.strptime(tomorrow, '%Y-%m-%d').strftime("%Y%m%d") > datetime.datetime.strptime(filedate, '%Y%m%d').strftime("%Y%m%d"): #Future filedate is ellegal
 			a += 1
+			print("pass ##filedate")
 		else:
 			sys.exit("ERROR 1.0.1: Illegal vcf file date")
 	elif line.startswith('##reference'):
-		if line.search('path_to_fasta', line)
+		if re.search(path_to_fasta, line):
 			a += 1
+			print("pass ##reference")
 		else:
 			sys.exit("ERROR 1.0.1: The entered fasta and the used fasta do not match")
-	elif line.startswith('##INFO'):
-		
-	elif line.startswith('##FORMAT'):
-
-	elif line.startswith('##FILTER'):
-	
-	elif line.startswith('#header'): #headerが不正でないか確認する
+	elif line.startswith('##INFO'): #later add
+		pass
+	elif line.startswith('##FORMAT'): #later add
+		pass
+	elif line.startswith('##FILTER'): #later add
+		pass
+	elif re.search('#CHROM',line): #header is illegal or not
 		a += 1
 		header_clm = line.split()
+		print("pass #header")
 		if header_clm[0] == '#CHROM':
 			b += 1
 		else:
+			print(header_clm[0])
 			sys.exit("ERROR 1.0.1: This vcf is not a standard format on #CHROM")
 		if header_clm[1] == 'POS':
 			b += 1
@@ -137,45 +139,49 @@ for line in vcf_data:
 			b += 1
 		else:
 			sys.exit("ERROR 1.0.1: This vcf is not a standard format on INFO")
+	elif line.startswith('##'):
+			pass
 	else:
-		if a != 4 or b != 8: #body に入るときにheader項目が揃っているか確認する
+		if a != 4 or b != 8: #Number of header items completery collected or not
+			print(a)
+			print(b)
 			sys.exit("ERROR 1.0.1: Something is missing in ##")
 		else:
 				fields = line.split()
-				chr_number = re.compile('\d+')
-				chr = chr_number.findall(fields[0])0
-				position = fields[1]
+				chr_number = fields[0]
+				chr = str(chr_number.replace('chr', ''))
+				position = str(fields[1])
 				if chr < curr_chr:
+
 					sys.exit("ERROR 1.0.1: Chromosome position is larger then chromosome size")
 				else:
-					if not position.isdecimal():
+					if not position.isdigit():
 						sys.exit("ERROR 1.0.1: positions should be numbers")
 					else:
 						pass
 					if chr == curr_chr:
-						if not position > previous_pos:
+						if position < previous_pos:
+							print (position)
+							print (previous_pos)
 							sys.exit("ERROR 1.0.1: Data are not sorted base on positions")
 						else:
 							previous_pos = fields[1]
 					else:
-						curr_chr = fields[0]
+						curr_chr = chr
 						previous_pos = fields[1]
 				if fields[3] == fields[4]:
 					sys.exit("ERROR 1.0.1: Reference allele and alternative alleles are the same")
-				if re.search('[^ATGC]', $fields[3]): #REF is only allowed for AGCT
+				if re.search('[^ATGC]', fields[3]): #REF is only allowed for AGCT
 					if fields[3] == "\." or fields[3] == "-": #Some submitter use "-" for Insertions
 						move_position = 1
 						fields[2] = fields[2] - 1
-						#hit_line = [fasta_line for fasta_line in fasta_file if fields[2] in fasta_line]
-						#split_hit_line = hit_line.split()
-						#add_allele = split_hit_line[]
 					else:
 						sys.exit("ERROR 1.0.1: Illegal characters are used in reference allele")
-				elif len($fields[3]) == 0 : #ref allele missing
+				elif len(fields[3]) == 0 : #ref allele missing
 					sys.exit("ERROR 1.0.1: Reference allele missing")
 				else:
 					pass
-				if re.search('[^ATGC]', $fields[4]) : #ALT is only allowed for AGCT
+				if re.search('[^ATGC]', fields[4]) : #ALT is only allowed for AGCT
 					if fields[4] == "\." or fields[4] == "-": #Some submitter use "-" for Insertions
 						move_position = 1
 						fields[2] = fields[2] - 1
@@ -184,34 +190,27 @@ for line in vcf_data:
 						#add_allele = split_hit_line[]
 					else:
 						sys.exit("ERROR 1.0.1: Illegal characters are used in alternative allele")
-				elif len($fields[4]) == 0 : #alt allele missing
+				elif len(fields[4]) == 0 : #alt allele missing
 					sys.exit("ERROR 1.0.1: Alternative allele missing")
 				else:
 					pass
 
-#CHRからINFOの前までの硬い部分のチェック
 
-#INFOにしようされているタグが##と一致するか確認する
+vcf_data.close()
 
-#実際のファイルとの比較をする
+#candidate of error
 
- 
-vcf_data.close() # 開いたファイルを閉じる
-
-
-#いれたら良さそうなエラー候補
-
-";ERR_LINE_EMPTY=Line is empty"
-";ERR_VRT_NOT_DEFINED=variation type not defined"
-";ERR_CHR_NOT_GROUPED=Data on the same chromosome are not grouped together"
-";ERR_CHR=invalid chromosome name"
-";ERR_VARI_LENGTH_INDEL=Indels with variable length alleles"
-";ERR_DUP_SITES=Duplicated sites"
-";ERR_SPACE_REF=Extra space before/after ref. allele"
+#";ERR_LINE_EMPTY=Line is empty"
+#";ERR_VRT_NOT_DEFINED=variation type not defined"
+#";ERR_CHR_NOT_GROUPED=Data on the same chromosome are not grouped together"
+#";ERR_CHR=invalid chromosome name"
+#";ERR_VARI_LENGTH_INDEL=Indels with variable length alleles"
+#";ERR_DUP_SITES=Duplicated sites"
+#";ERR_SPACE_REF=Extra space before/after ref. allele"
 
 
-	    if((substr($alleles[$i], 0, 1) ne substr($fields[3], 0, 1)) && (length($fields[3]) > 1 || length($alleles[$i]) > 1)) {
-		$current_line_error = 1;
-		$self->{result}->{ERR}->{ERR_INVALID_INDEL_MNV_FMT}->{counter}++;
-		$line .= ";ERR_INVALID_INDEL_MNV_FMT=Ref and alt alleles don not have common leading base";
-	    }
+#	    if((substr($alleles[$i], 0, 1) ne substr($fields[3], 0, 1)) && (length($fields[3]) > 1 || length($alleles[$i]) > 1)) {
+#		$current_line_error = 1;
+#		$self->{result}->{ERR}->{ERR_INVALID_INDEL_MNV_FMT}->{counter}++;
+#		$line .= ";ERR_INVALID_INDEL_MNV_FMT=Ref and alt alleles don not have common leading base";
+#	    }
