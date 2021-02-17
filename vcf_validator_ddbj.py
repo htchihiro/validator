7a67a6fe7b59b3c7113af51e893d9a636d0076e0
htchihiro <chata@nig.ac.jp>
2021-02-17 14:17:23 +0900

    Fix the logic of comparing chromosome numbers

--- vcf_validator_ddbj.py
+++ vcf_validator_ddbj.py
@@ -43,11 +43,11 @@ start_time = time.time()
 vcf_exist = os.path.isfile(vcffile)
 if vcf_exist:
 	if os.path.getsize(vcffile) == 0:
-		sys.exit("ERROR 1.0.1: vcf file is empty")
+		sys.exit("ERROR JV_VR0007: vcf file is empty")
 	else:
 		pass
 else:
-	sys.exit("ERROR 1.0.1: vcf file is blank or not a file")
+	sys.exit("ERROR JV_VR0008: vcf file is blank or not a file")
 
 #gi_exist = os.path.isfile(gi_list_file)
 #if gi_exist:
@@ -59,7 +59,7 @@ path_fasta_exist = os.path.isfile(fasta_for_validation)
 if path_fasta_exist:
 	pass
 else:
-	sys.exit("ERROR 1.0.1: fasta file is blank or not a file")
+	sys.exit("ERROR JV_VR0010: fasta file is blank or not a file")
 
 #Cheking a imput score of skip_dense
 
@@ -109,7 +109,7 @@ for line in vcf_lines:
 			a += 1
 			print("pass ##fileformat")
 		else:
-			print(str(line_numbers) + " WARNING 1.0.1: Illegal vcf version")
+			print(str(line_numbers) + " WARNING JV_VR0012: Illegal vcf version")
 	elif line.startswith('##fileDate'):
 		#import datetime
 		#tomorrow = str(datetime.date.today() + datetime.timedelta(days=1))
@@ -125,7 +125,7 @@ for line in vcf_lines:
 			a += 1
 			print("pass ##reference")
 		else:
-			print(str(line_numbers) + " WARNING 1.0.1: The entered fasta and the used fasta do not match")
+			print(str(line_numbers) + " WARNING JV_VR0014: The entered fasta and the used fasta do not match")
 	elif line.startswith('##INFO'): #later add
 		pass
 	elif line.startswith('##FORMAT'): #later add
@@ -140,78 +140,75 @@ for line in vcf_lines:
 			b += 1
 		else:
 			print(header_clm[0])
-			print(str(line_numbers) + " WARNING 1.0.1: This vcf is not a standard format on CHROM")
+			print(str(line_numbers) + " WARNING JV_VR0015: This vcf is not a standard format on CHROM")
 		if header_clm[1] == 'POS':
 			b += 1
 		else:
-			print(str(line_numbers) + " WARNING 1.0.1: This vcf is not a standard format on POS")
+			print(str(line_numbers) + " WARNING JV_VR0016: This vcf is not a standard format on POS")
 		if header_clm[2] == 'ID':
 			b += 1
 		else:
-			print(str(line_numbers) + " WARNING 1.0.1: This vcf is not a standard format on ID")
+			print(str(line_numbers) + " WARNING JV_VR0017: This vcf is not a standard format on ID")
 		if header_clm[3] == 'REF':
 			b += 1
 		else:
-			print(str(line_numbers) + " WARNING 1.0.1: This vcf is not a standard format on REF")
+			print(str(line_numbers) + " WARNING JV_VR0018: This vcf is not a standard format on REF")
 		if header_clm[4] == 'ALT':
 			b += 1
 		else:
-			print(str(line_numbers) + " WARNING 1.0.1: This vcf is not a standard format on ALT")
+			print(str(line_numbers) + " WARNING JV_VR0019: This vcf is not a standard format on ALT")
 		if header_clm[5] == 'QUAL':
 			b += 1
 		else:
-			print(str(line_numbers) + " WARNING 1.0.1: This vcf is not a standard format on QUAL")
+			print(str(line_numbers) + " WARNING JV_VR0020: This vcf is not a standard format on QUAL")
 		if header_clm[6] == 'FILTER':
 			b += 1
 		else:
-			print(str(line_numbers) + " WARNING 1.0.1: This vcf is not a standard format on FILTER")
+			print(str(line_numbers) + " WARNING JV_VR0021: This vcf is not a standard format on FILTER")
 		if header_clm[7] == 'INFO':
 			b += 1
 		else:
-			print(str(line_numbers) + " WARNING 1.0.1: This vcf is not a standard format on INFO")
+			print(str(line_numbers) + " WARNING JV_VR0022: This vcf is not a standard format on INFO")
 	elif line.startswith('##'):
 			pass
 	else:
 		if a != 4 or b != 8: #Number of header items completery collected or not
 			print(a)
 			print(b)
-			sys.exit(str(line_numbers) + "ERROR 1.0.1: Something is missing in header")
+			sys.exit(str(line_numbers) + "ERROR JV_VR0023: Something is missing in header")
 		else:
 				fields = line.split()
 				chr_number = fields[0]
 				chr = str(chr_number.replace('chr', ''))
 				position = str(fields[1])
-				if int(chr) < int(curr_chr):
-					print(str(line_numbers) + " WARNING 1.0.1: Chromosome numbers were not sorted")
-				else:
-					if not position.isdigit():
-						if chr != X or chr != Y:
-							pass
-						else:
-							print(str(line_numbers) + " WARNING 1.0.1: positions should be numbers")
-					else:
+				if not position.isdigit():
+					if chr == X or chr == Y:
 						pass
-					if chr == curr_chr:
-						if int(position) < int(previous_pos):
-							print ("previous:" + position)
-							print ("now:" + previous_pos)
-							print(str(line_numbers) + " WARNING 1.0.1: Data are not sorted base on positions")
-						else:
-							previous_pos = fields[1]
 					else:
-						curr_chr = chr
+						print(str(line_numbers) + " WARNING JV_VR0025: positions should be numbers")
+				elif int(chr) < int(curr_chr):
+						print(str(line_numbers) + " WARNING JV_VR0024: Chromosome numbers were not sorted")
+				if chr == curr_chr:
+					if int(position) < int(previous_pos):
+						print ("previous:" + position)
+						print ("now:" + previous_pos)
+						print(str(line_numbers) + " WARNING JV_VR0026: Data are not sorted base on positions")
+					else:
 						previous_pos = fields[1]
+				else:
+					curr_chr = chr
+					previous_pos = fields[1]
 				if fields[3] == fields[4]:
-					print(str(line_numbers) + " WARNING 1.0.1: Reference allele and alternative alleles are the same")
+					print(str(line_numbers) + " WARNING JV_VR0027: Reference allele and alternative alleles are the same")
 				if re.search('[^ATGC]', fields[3]): #REF is only allowed for AGCT
 					if re.search('[MRWSYKVHDBN]', fields[3]) :
-						print(str(line_numbers) + " WARNING 1.0.1: No non-ATGC nucleotide or iupac ambiguity codes (ie. R, Y, etc.) are in either reference alleles")
+						print(str(line_numbers) + " WARNING JV_VR0028: No non-ATGC nucleotide or iupac ambiguity codes (ie. R, Y, etc.) are in either reference alleles")
 					elif fields[3] == "\." or fields[3] == "-": #Some submitter use "-" for Insertion
-						print(str(line_numbers) + " WARNING 1.0.1: This vcf file uses - or . for reference colum")
+						print(str(line_numbers) + " WARNING JV_VR0029: This vcf file uses - or . for reference colum")
 					else:
-						print(str(line_numbers) + " WARNING 1.0.1: Illegal characters are used in reference allele")
+						print(str(line_numbers) + " WARNING JV_VR0030: Illegal characters are used in reference allele")
 					if len(fields[4]) > 50:
-						print(str(line_numbers) + " WARNING 1.0.1: Insertion size is over 50bp")
+						print(str(line_numbers) + " WARNING JV_VR0031: Insertion size is over 50bp")
 					else:
 						pass
 						#move_position = 1 #If we need to fix vcf files for standerd, add correction steps
@@ -229,7 +226,7 @@ for line in vcf_lines:
 						#ref_out.close()
 						#rm ref_out					
 				elif len(fields[3]) == 0 : #ref allele missing
-					print(str(line_numbers) + " WARNING 1.0.1: Reference allele missing")
+					print(str(line_numbers) + " WARNING JV_VR0032: Reference allele missing")
 				else:
 					if len(fields[3]) > len(fields[4]): #For deletion cases
 						back_one = int(fields[1]) + int(len(fields[3])) - 1
@@ -252,19 +249,19 @@ for line in vcf_lines:
 							fn_search = re.search(r'([ATGC]+)', fasta_nuc)
 							fn_ex = fn_search.group()
 							if str(fields[3]) != str(fn_ex):
-								print(str(line_numbers) + "_vcf:" +str(fields[3]) + "_ref:" +str(fn_ex) + " WARNING 1.0.1: Dosen't match reference allele")
+								print(str(line_numbers) + "_vcf:" +str(fields[3]) + "_ref:" +str(fn_ex) + " WARNING JV_VR0033: Dosen't match reference allele")
 					ref_bed = ""
 				if re.search('[^ATGC]', fields[4]) : #ALT is only allowed for AGCT
 					if re.search('[MRWSYKVHDBN]', fields[4]) :
-						print(str(line_numbers) + " WARNING 1.0.1: No non-ATGC nucleotide or iupac ambiguity codes (ie. R, Y, etc.) are in alternative alleles")
+						print(str(line_numbers) + " WARNING JV_VR0034: No non-ATGC nucleotide or iupac ambiguity codes (ie. R, Y, etc.) are in alternative alleles")
 					elif fields[4] == "\." or fields[4] == "-": #Some submitter use "-" for Deletion
-						print(str(line_numbers) + " WARNING 1.0.1: This vcf file uses - or . for alternative colum")
+						print(str(line_numbers) + " WARNING JV_VR0035: This vcf file uses - or . for alternative colum")
 					if re.search(',', fields[4]) : #For multi-allelic
-						print(str(line_numbers) + " WARNING 1.0.1: Please sepalate the multi-allelic")
+						print(str(line_numbers) + " WARNING JV_VR0036: Please sepalate the multi-allelic")
 					else:
-						print(str(line_numbers) + " WARNING 1.0.1: Illegal characters are used in alternative allele")
+						print(str(line_numbers) + " WARNING JV_VR0037: Illegal characters are used in alternative allele")
 					if len(fields[3]) > 50:
-						print(str(line_numbers) + " WARNING 1.0.1: Deletion size is over 50bp")
+						print(str(line_numbers) + " WARNING JV_VR0038: Deletion size is over 50bp")
                         #move_position = 1 #If we need to fix vcf files for standerd, add correction steps
 						#new_position = fields[2] - 1
 						#ref_bed = print ( chr"\t"new_position-1"\t"new_position)
@@ -280,7 +277,7 @@ for line in vcf_lines:
 						#ref_out.close()
 						#rm ref_out
 				if len(fields[4]) == 0 : #alt allele missing
-					print(str(line_numbers) + " WARNING 1.0.1: Alternative allele missing")
+					print(str(line_numbers) + " WARNING JV_VR0039: Alternative allele missing")
 else:
 	print (str(line_numbers) + " All lines of vcf were validated! Please check errors and/or warnings in the log file")
 
